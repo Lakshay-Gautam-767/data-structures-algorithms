dd26a5d5b07c7f720b5b9ed748771b932051d7c5
Lakshay Gautam <lakshaygautam7309@gmail.com>
2026-05-23 21:18:36 +0530

    theory added

--- 1. First.cpp
+++ 1. First.cpp
@@ -1,6 +1,6 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-    cout << "Namaste Bharat";
-}
+#include <iostream>  //Import the file that contains cout (printing)
+using namespace std; // Use the standard namespace
+int main(){ // Starting point of our program
+    cout << "Namaste Bharat" << endl; // cout means print out something
+    cout << "\n"; // endl or '\n' means go to the next line
+}  // {} Everyting inside these belongs to main
