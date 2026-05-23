b2da04d571a606cd661039a163ddaa7db6fa5db0
Lakshay Gautam <lakshaygautam7309@gmail.com>
2026-05-23 21:41:37 +0530

    simple I/O example

---
+++ 2. input_output.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int a;
+    cout << "Enter a number: " << endl;
+    // taking input
+    cin >> a;
+    cout << "You entered " << a <<endl;
+}
